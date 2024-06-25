d457eb0cd01e91725872f7faabc49c6e8748245d
Kev9180 <kev9180@gmail.com>
2024-06-25 16:41:00 -0700

    Add splash screen with intro text

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Welcome to my personal website!');
-});
+let intro = document.querySelector('.intro');
+let introHeader = document.querySelector('.intro-header');
+let splashSpan = document.querySelectorAll('.splash');
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        splashSpan.forEach((span, index) => {
+            // Set a timeout to handle how quickly the two welcome text spans will appear ("Hi there!" and "I'm Kevin")
+            setTimeout( () => {
+                span.classList.add('active');
+            }, (index + 1) * 600)
+        });
+
+        // Set a timeout for the welcome prompt text - how long it will be displayed on the screen
+        setTimeout(() => {
+            splashSpan.forEach((span, index) => {
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (index + 1) * 50)
+            })
+        }, 2000)    // 2 seconds
+
+
+        setTimeout(() => {
+            intro.style.top = '-100vh';
+        }, 2300)
+    })
+})
